f6e76a6dee63680b3170af2da8f919555a9eafd2
Elysian4 <niyati.4gupta@gmail.com>
2024-01-30 22:53:53 +0530

    added files

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+function toggleNavbar() {
+  var navbarNav = document.getElementById('navbarNav');
+  navbarNav.classList.toggle('active');
+  var navbarToggler = document.querySelector('.navbar-toggler');
+  navbarToggler.classList.toggle('active');
+}
